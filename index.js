1ea23b11a286c3de6f348a640e0b3db3c6776b49
Riad Shalahin Leon <105214634+riadleon@users.noreply.github.com>
2022-11-10 20:46:17 +0600

    used the atlas

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = 'mongodb://localhost:27017';
+// const uri = 'mongodb://localhost:27017';
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.afq9fgb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 function toVerifyJWT(req, res, next) {
